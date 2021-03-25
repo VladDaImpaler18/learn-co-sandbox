ff6774325012df3cc3eee31286b75ddd91940289
Vladimir J <vladdaimpaler@protonmail.com>
2021-03-25 15:28:28 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Album
+  
+  @@album_count = 0
+  
+  def initialize
+    @@album_count += 1
+  end
+  
+  def release_date=(date)
+    @release_date = date
+  end
+  
+  def release_date
+    @release_date
+  end
+
+  def self.count
+    @@album_count
+  end
+end
